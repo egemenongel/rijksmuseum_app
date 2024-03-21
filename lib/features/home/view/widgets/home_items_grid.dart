3f9b82d27b6435fa36d884d272d9a0cee75a0b69
egemenongel <egemenongel@hotmail.com>
2024-03-21 17:02:48 +0300

    refactor: Refactor home view

---
+++ lib/features/home/view/widgets/home_items_grid.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:rijksmuseum_app/features/home/models/home_response_model.dart';
+import 'package:rijksmuseum_app/features/home/view/widgets/home_item_card.dart';
+import 'package:rijksmuseum_app/features/home/viewmodel/home_providers.dart';
+import 'package:rijksmuseum_app/features/home/viewmodel/home_viewmodel.dart';
+
+class ItemsGrid extends HookConsumerWidget {
+  const ItemsGrid({
+    super.key,
+    required this.items,
+  });
+
+  final List<ArtObjectModel> items;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final sc = useScrollController();
+    sc.addListener(() async {
+      if (!ref.read(isFetchingProvider)) {
+        if (sc.position.pixels >= sc.position.maxScrollExtent) {
+          await ref.read(homeViewmodel.notifier).fetchNextList();
+        }
+      }
+    });
+    return GridView.builder(
+      padding: EdgeInsets.zero,
+      controller: sc,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.75,
+      ),
+      itemCount: items.length,
+      itemBuilder: (_, index) {
+        return HomeItemCard(
+          artItem: items[index],
+        );
+      },
+    );
+  }
+}
