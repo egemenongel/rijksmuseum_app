5468dee6bba1374c9858c38e3dea8e1d555336c8
egemenongel <egemenongel@hotmail.com>
2024-03-21 16:43:21 +0300

    test: Write tests for home service

---
+++ test/features/home/home_service_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:rijksmuseum_app/features/home/models/home_response_model.dart';
+
+import '../../mock/mocks.dart';
+
+void main() {
+  test('Get home items', () async {
+    final testService = MockHomeService();
+    when(() => testService.fetchHomeItems())
+        .thenAnswer((_) async => const HomeResponseModel(artObjects: []));
+
+    final res = await testService.fetchHomeItems();
+
+    verify(() => testService.fetchHomeItems()).called(1);
+
+    expect(res, isA<HomeResponseModel>());
+  });
+}
---
+++ test/mock/mocks.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:rijksmuseum_app/features/home/service/home_service.dart';
+
+class MockHomeService extends Mock implements IHomeService {}
